5e1f4843c1548a70268c4df417ee91b0d4d6eb2f
unknown <dborel@dborel-bc.TheFacebook.com>
2015-01-19 06:17:09 -0800

    - Initializing num_buttons. - Opening new connection when "*" prefix is specified.

--- vrpn_Button.C
+++ vrpn_Button.C
@@ -500,7 +500,15 @@ vrpn_Threshold_Button_Server::vrpn_Threshold_Button_Server(const char *name, vrp
 	, _threshold(threshold)
 	, _analog(NULL)
 {
-	_analog = new vrpn_Analog_Remote(name, c);
+	num_buttons = 1;
+
+	// If the name starts with the '*' character, use the server
+	// connection rather than making a new one.
+	if (name[0] == '*')
+		_analog = new vrpn_Analog_Remote(&name[1]);
+	else
+		_analog = new vrpn_Analog_Remote(name, c);
+
 	_analog->register_change_handler(this, handle_analog_update);
 }
 
